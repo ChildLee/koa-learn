448aa58b017f43479020aa0cad233ef2d5a876ee
ChildLee <471227142@qq.com>
2018-06-02 12:49:54 +0800

    commit

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,10 @@
-import query from './mysql'
+import query from './db/mysql'
+import query1 from './db/mysql1'
 
 query('select * from provinces where code=? limit ? ', [11, 1]).then(res => {
+    console.log(res)
+})
+
+query1('select * from area_provinces where code=? limit ? ', [11, 1]).then(res => {
     console.log(res)
 })
