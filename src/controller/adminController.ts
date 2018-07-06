de48c41fb0619ef6244a9b1314705817540d9e7a
ChildLee <471227142@qq.com>
2018-07-06 13:28:24 +0800

    关闭sharp缓存,会导致删除文件失败

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -33,34 +33,28 @@ class AdminController {
 
 //文件处理
 function writeFile(file) {
-    const image = sharp(file.path)
+    //打开缓存会导致删除文件报错
+    sharp.cache(false)
+    let image = sharp(file.path)
     const newPath = path.join(config.static, dateformat('yyyymmddHHMMss') + Utils.randomStr() + '.jpg')
     image.metadata().then(res => {
         //小于该值不打水印
         if (res.width > 400 && res.height > 100) {
-            image.overlayWith(config.watermark, {left: 10000, top: 10000})
-                .toFile(newPath)
-                .then(res => {
-                    delFile(file.path)
-                })
+            return image.overlayWith(config.watermark, {left: 10000, top: 10000})
         } else {
-            image.toFile(newPath)
-                .then(res => {
-                    delFile(file.path)
-                })
+            return image
         }
+    }).then(res => {
+        return res.toFile(newPath)
+    }).then(res => {
+        if (res) delFile(file.path)
     })
-
 }
 
+//删除文件
 function delFile(path) {
     fs.unlink(path, err => {
-        if (err) {
-            console.log(err)
-            setTimeout(() => {
-                fs.unlink(path,()=>{})
-            }, 1000)
-        }
+        if (err) console.log(err)
     })
 }
 
