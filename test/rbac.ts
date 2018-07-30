1941641a8a839f76f8a3210870259ff3a98c13c2
ChildLee <471227142@qq.com>
2018-07-30 20:34:23 +0800

    完善rbac

--- test/rbac.ts
+++ test/rbac.ts
@@ -43,12 +43,6 @@ const User = sequelize.define('user', {
     defaultValue: '',
     comment: '姓名'
   },
-  isAdmin: {
-    type: BOOLEAN,
-    allowNull: false,
-    defaultValue: true,
-    comment: '是否是超级管理员'
-  },
   status: {
     type: BOOLEAN,
     allowNull: false,
@@ -86,137 +80,191 @@ const Permission = sequelize.define('permission', {
   type: {
     type: TINYINT,
     allowNull: false,
-    defaultValue: -1,
-    comment: '资源类型：-1=default,0=menu,1=button'
+    defaultValue: 1,
+    comment: '资源类型：-1=root,0=menu,1=operations'
   },
-  menu_id: {
+  name: {
+    type: STRING,
+    allowNull: false,
+    defaultValue: '',
+    comment: '权限名称'
+  },
+  url: {
+    type: STRING,
+    allowNull: false,
+    defaultValue: '',
+    comment: '权限URL'
+  },
+  pid: {
     type: INTEGER,
-    comment: '菜单ID'
+    comment: '父权限ID'
+  },
+  order: {
+    type: STRING(5),
+    allowNull: false,
+    defaultValue: '',
+    comment: '排序'
   }
 }, {
   tableName: 'sys_permission',
   comment: '权限表'
 })
-
-const Menu = sequelize.define('menu', {
-  id: {
-    type: INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: STRING,
-    allowNull: false,
-    defaultValue: '',
-    comment: '菜单名称'
-  },
-  url: {
-    type: STRING,
-    allowNull: false,
-    defaultValue: '',
-    comment: '菜单URL'
-  },
-  pid: {
-    type: INTEGER,
-    comment: '父菜单ID'
-  }
-}, {
-  tableName: 'sys_menu',
-  comment: '菜单表'
-})
-
 //菜单表-父菜单与主键关联
-Menu.hasMany(Menu, {foreignKey: 'pid'})
-
-const Operation = sequelize.define('operation', {
-  id: {
-    type: INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: STRING,
-    allowNull: false,
-    defaultValue: ''
-  },
-  method: {
-    type: STRING,
-    allowNull: false,
-    defaultValue: ''
-  },
-  url: {
-    type: STRING,
-    allowNull: false,
-    defaultValue: ''
-  }
-}, {
-  tableName: 'sys_operation',
-  comment: '操作权限'
-})
-
+Permission.hasMany(Permission, {foreignKey: 'pid'})
 
 //用户-角色表
-// const UserRole = sequelize.define('user_role', {})
-User.belongsToMany(Role, {through: 'sys_user_role'})
-Role.belongsToMany(User, {through: 'sys_user_role'})
+const userRole = sequelize.define('user_role', {}, {
+  tableName: 'sys_user_role',
+  //不添加时间戳属性
+  timestamps: false,
+  comment: '用户-角色关联表'
+})
+User.belongsToMany(Role, {through: userRole})
+Role.belongsToMany(User, {through: userRole})
 
 //角色-权限表
-// const RoleAccess = sequelize.define('role_permission', {})
-Role.belongsToMany(Permission, {through: 'sys_role_permission'})
-Permission.belongsToMany(Role, {through: 'sys_role_permission'})
+const RolePermission = sequelize.define('role_permission', {}, {
+  tableName: 'sys_role_permission',
+  //不添加时间戳属性
+  timestamps: false,
+  comment: '角色-权限关联表'
+})
+Role.belongsToMany(Permission, {through: RolePermission})
+Permission.belongsToMany(Role, {through: RolePermission})
 
-//权限-菜单关联
-Permission.belongsTo(Menu)
-Menu.hasOne(Permission)
-
-//权限-操作关联
-Permission.belongsTo(Operation)
-Operation.hasOne(Permission)
 it('init', async () => {
   await sequelize.sync({force: true})
   //菜单数据初始化
-  const type = 0//类型为菜单
-  await Menu.create({name: '菜单'})
+  await Permission.create({name: '权限'})
   //一级菜单
-  await Menu.create({name: '用户管理', pid: 1, permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '角色管理', pid: 1, permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '权限管理', pid: 1, permission: {type}}, {include: [Permission]})
+  const m1 = await Permission.create({name: '用户管理', pid: 1, type: 0, order: 97})//pid:2
+  const m2 = await Permission.create({name: '角色管理', pid: 1, type: 0, order: 98})//pid:3
+  const m3 = await Permission.create({name: '权限管理', pid: 1, type: 0, order: 99})//pid:4
+  const m4 = await Permission.create({name: '商品管理', pid: 1, type: 0, order: 0})//pid:5
+  //一级菜单按钮
+  const b1 = await Permission.create({name: '文件管理', pid: 1, type: 1, order: 1})//pid:6
   //用户菜单
-  await Menu.create({name: '用户列表', pid: 2, url: '/user/list', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '添加用户', pid: 2, url: '/user/add', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '编辑用户', pid: 2, url: '/user/update', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '设置角色', pid: 2, url: '/user/role', permission: {type}}, {include: [Permission]})
+  const access4 = await Permission.create({name: '用户列表', pid: 2, url: '/user/list', type: 0})
+  const access5 = await Permission.create({name: '添加用户', pid: 2, url: '/user/add', type: 0})
+  const access6 = await Permission.create({name: '编辑用户', pid: 2, url: '/user/update', type: 0})
+  const access7 = await Permission.create({name: '设置角色', pid: 2, url: '/user/role', type: 0})
   //角色菜单
-  await Menu.create({name: '角色列表', pid: 3, url: '/role/list', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '添加角色', pid: 3, url: '/role/add', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '编辑角色', pid: 3, url: '/role/update', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '设置权限', pid: 3, url: '/role/permission', permission: {type}}, {include: [Permission]})
+  const access8 = await Permission.create({name: '角色列表', pid: 3, url: '/role/list', type: 0})
+  const access9 = await Permission.create({name: '添加角色', pid: 3, url: '/role/add', type: 0})
+  const access10 = await Permission.create({name: '编辑角色', pid: 3, url: '/role/update', type: 0})
+  const access11 = await Permission.create({name: '设置权限', pid: 3, url: '/role/permission', type: 0})
   //权限菜单
-  await Menu.create({name: '权限列表', pid: 4, url: '/permission/list', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '添加权限', pid: 4, url: '/permission/add', permission: {type}}, {include: [Permission]})
-  await Menu.create({name: '编辑权限', pid: 4, url: '/permission/update', permission: {type}}, {include: [Permission]})
+  const access12 = await Permission.create({name: '权限列表', pid: 4, url: '/permission/list', type: 0})
+  const access13 = await Permission.create({name: '添加权限', pid: 4, url: '/permission/add', type: 0})
+  const access14 = await Permission.create({name: '编辑权限', pid: 4, url: '/permission/update', type: 0})
   //
-  const user = await User.create({name: 'admin'})
-  const role = await Role.create({name: 'SuperAdmin'})
-  await user['addRoles'](role)
+  const access15 = await Permission.create({name: '商品列表', pid: 5, url: '/commodity/list', type: 0})
+  //
+  const btn1 = await Permission.create({name: 'logo上传', pid: 6, url: '/upload/logo', type: 1})
+  //
+  const user1 = await User.create({name: '用户一'})
+  const user2 = await User.create({name: '用户二'})
+  const user3 = await User.create({name: '用户三'})
+  //
+  const SuperRole = await Role.create({name: '超级管理员'})
+  const role1 = await Role.create({name: 'Admin'})
+  const role2 = await Role.create({name: 'SuperUser'})
+  const role3 = await Role.create({name: 'User'})
+  //
+  await role1['addPermissions']([m1, m2, m3, access4, access5, access6, access7, access8, access9, access10, access11, access12, access13, access14])
+  await role2['addPermissions']([m1, access4, access5, access6])
+  await role3['addPermissions']([m4, b1, access15, btn1])
+  //
+  await user1['addRoles']([role1, role2])
+  await user2['addRoles']([role2])
+  await user3['addRoles']([role3])
 })
 
-it('menu', async () => {
-  //菜单生成
-  const menu = await Menu.findAll({
+//所有权限菜单
+it('MenuAll', async () => {
+  const menu = await Permission.findAll({
+    attributes: ['id', 'name'],
     where: {pid: 1},
+    order: ['order'],
     include: [{
-      model: Menu,
+      model: Permission,
+      attributes: ['id', 'name', 'url']
+    }]
+  })
+  console.log(JSON.parse(JSON.stringify(menu)))
+})
+
+//角色拥有的权限菜单
+it('UserMenuAll', async () => {
+  const menu = await Permission.findAll({
+    include: [{
+      model: Role,
+      attributes: [],
+      where: {id: 2}
+    }, {
+      model: Permission,
       attributes: ['id', 'name', 'url']
     }],
-    attributes: ['id', 'name']
+    attributes: ['id', 'name'],
+    where: {pid: 1}
   })
-  console.log(JSON.stringify(menu, null, 2))
+  console.log(JSON.parse(JSON.stringify(menu)))
 })
 
-it('findAll', function () {
-  Menu.findAll().then(res => {
-    res = JSON.parse(JSON.stringify(res))
-    console.log(res)
+//用户菜单
+it('Menus', async () => {
+  //查询用户所有的角色
+  const roles = await userRole.findAll({
+    raw: true,
+    attributes: ['role_id'],
+    where: {user_id: 1}
   })
+  //没有角色返回空数组
+  if (!roles.length) {
+    return []
+  }
+  const role = []
+  for (let i = 0; i < roles.length; i++) {
+    role.push(roles[i]['role_id'])
+  }
+  //查询所有角色菜单
+  const menus = await Permission.findAll({
+    attributes: ['id', 'name'],
+    where: {type: 0, pid: 1},
+    order: ['order'],
+    include: [{
+      model: Role,
+      attributes: [],
+      where: {
+        id: {[Op.in]: role}
+      }
+    }, {
+      model: Permission,
+      attributes: ['id', 'name', 'url']
+    }]
+  })
+  console.log(JSON.parse(JSON.stringify(menus)))
 })
+
+//url访问权限查询
+it('Access', async () => {
+  const access = await Role.findOne({
+    attributes: [],
+    include: [{
+      model: User,
+      attributes: [],
+      where: {id: 3}
+    }, {
+      model: Permission,
+      attributes: ['url'],
+      where: {url: '/upload/logo'}
+    }]
+  })
+  console.log(JSON.stringify(access, null, 2))
+})
+
+//封号
+it('Banned Account', async () => {
+  const status = await User.update({status: 0}, {where: {id: 1}})
+  console.log(status)
+}) 
